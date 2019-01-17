8081a9f1dd4d7f9a2d8479df0ba12a927b359ce7
Dowon Cha <dowoncha.cs@gmail.com>
2019-01-17 15:47:15 -0500

    Updates

---
+++ precache-manifest.c011c5a9167b83c6c7a4604cefc38e23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f31f65fd62833a5206ff",
+    "url": "/ui-studio/static/css/main.a7ffbdc0.chunk.css"
+  },
+  {
+    "revision": "f31f65fd62833a5206ff",
+    "url": "/ui-studio/static/js/main.f31f65fd.chunk.js"
+  },
+  {
+    "revision": "99a24f742168b5b3abc7",
+    "url": "/ui-studio/static/js/1.99a24f74.chunk.js"
+  },
+  {
+    "revision": "b3e7f66419837aaec9cb",
+    "url": "/ui-studio/static/js/runtime~main.b3e7f664.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ui-studio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2acb07610fcb577f4c3943e51e536bb2",
+    "url": "/ui-studio/index.html"
+  }
+];
