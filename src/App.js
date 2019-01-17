8f4ace84cea9bbe7e859166a3ff68fd2159622d1
Dowon Cha <dowoncha.cs@gmail.com>
2019-01-17 15:40:38 -0500

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
+
+import TypographyPage from './TypographyPage'
+
+function HomePage({ ...props }) {
+  return (
+    <div>
+      <ul>
+        <li><Link to="/cozy">Cozy Design Page</Link></li>
+        <li><Link to="/typography">Typography</Link></li>
+      </ul>
+    </div>
+  )
+}
+
+const sandyBeige = 'rgb(251,250,246)'
+
+function CozyPage({ ...props }) {
+  return (
+    <div style={{ width: '100vw', height: '100vh', backgroundColor: sandyBeige }}>
+      <h1 style={{ textAlign: 'center' }}>Cozy Page</h1>
+    </div>
+  )
+}
+
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Router>
+          <Switch>
+            <Route path="/" exact component={HomePage} />
+            <Route path="/typography" component={TypographyPage} />
+            <Route path="/cozy" component={CozyPage} /> 
+            <Route render={props => <div>404</div>} />
+          </Switch>
+        </Router>
       </div>
     );
   }
